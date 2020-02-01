8885686c4c1c8ca0ab312bb69a8ca96a7ddcf453
srivastavakash <srivastavakash239@gmail.com>
2020-02-01 14:36:26 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Hello name={this.state.name} />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-m5ayeu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-m5ayeu)
